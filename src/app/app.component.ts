d2f81d3d948e36c24a5815fab23fa8b6ee9adfa5
vc27xo <chandran.agilan@ing.com>
2020-05-02 22:16:51 +0200

    Added button to home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Item, ApiService } from './services/api.service';
 
 @Component({
@@ -9,6 +9,8 @@ import { Item, ApiService } from './services/api.service';
 export class AppComponent implements OnInit {
   title = 'angular-pwa';
   items: Array<Item>;
+  deferredPrompt: any;
+  showButton = false;
 
   constructor(private apiService: ApiService) { }
 
@@ -23,5 +25,33 @@ export class AppComponent implements OnInit {
       }, (err) => {
         console.log(err);
       });
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          this.deferredPrompt = null;
+        });
+    }
   }
 }
